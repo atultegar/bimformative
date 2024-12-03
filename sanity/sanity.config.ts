85a3dd97d157520473a10fd059e0e0178d3ad7d7
Atul Tegar <atul.tegar@gmail.com>
2024-12-03 12:56:06 +0100

    general updates 4

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -5,6 +5,8 @@ import {schemaTypes} from './schemaTypes'
 import { codeInput } from '@sanity/code-input'
 import {youtubeInput} from 'sanity-plugin-youtube-input';
 
+const googleApiKey = process.env.GOOGLE_API_KEY || '';
+
 export default defineConfig({
   name: 'default',
   title: 'bimformative',
@@ -12,7 +14,7 @@ export default defineConfig({
   projectId: 'wlb0lt21',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool(), codeInput(), youtubeInput({apiKey: 'AIzaSyBQv1wtnwx-pkjvrQNQSFLuhDSLRVOBsbY'})],
+  plugins: [structureTool(), visionTool(), codeInput(), youtubeInput({apiKey: googleApiKey})],
 
   schema: {
     types: schemaTypes,
